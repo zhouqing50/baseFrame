ffd238845a557d871915d8ad24272cf0bca33a83
zhouqing <zhouqing50>
2016-04-03 11:33:11 +0800

    新增 netty http

--- src/test/java/netty/echo/http/HttpHelloWorldServerHandler.java
+++ src/test/java/netty/echo/http/HttpHelloWorldServerHandler.java
@@ -15,25 +15,29 @@
  */
 package netty.echo.http;
 
-import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
-import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
-import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
-import static io.netty.handler.codec.http.HttpResponseStatus.CONTINUE;
-import static io.netty.handler.codec.http.HttpResponseStatus.OK;
-import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFutureListener;
+import io.netty.buffer.UnpooledByteBufAllocator;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpHeaderUtil;
-import io.netty.handler.codec.http.HttpHeaderValues;
-import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.util.Map;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 
 public class HttpHelloWorldServerHandler extends ChannelHandlerAdapter {
-    private static final byte[] CONTENT = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd' };
+
+    private ByteBuf buffer_body = UnpooledByteBufAllocator.DEFAULT.buffer();
+    /*
+     * for debug
+     */
+    private StringBuffer sb_debug = new StringBuffer();
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
@@ -43,23 +47,45 @@ public class HttpHelloWorldServerHandler extends ChannelHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         if (msg instanceof HttpRequest) {
-            HttpRequest req = (HttpRequest) msg;
-
-            if (HttpHeaderUtil.is100ContinueExpected(req)) {
-                ctx.write(new DefaultFullHttpResponse(HTTP_1_1, CONTINUE));
+            HttpRequest request = (HttpRequest) msg;
+            sb_debug.append("\n>> HTTP REQUEST -----------\n");
+            sb_debug.append(request.protocolVersion().toString())
+                    .append(" ").append(request.method().name())
+                    .append(" ").append(request.uri());
+            sb_debug.append("\n");
+            HttpHeaders headers = request.headers();
+            if (!headers.isEmpty()) {
+                for (Map.Entry<CharSequence, CharSequence> header : headers) {
+                    sb_debug.append(header.getKey()).append(": ").append(header.getValue()).append("\n");
+                }
             }
-            boolean keepAlive = HttpHeaderUtil.isKeepAlive(req);
-            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(CONTENT));
-            response.headers().set(CONTENT_TYPE, "text/plain");
-            response.headers().setInt(CONTENT_LENGTH, response.content().readableBytes());
-
-            if (!keepAlive) {
-                ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-            } else {
-                response.headers().set(CONNECTION, HttpHeaderValues.KEEP_ALIVE);
-                ctx.write(response);
+            sb_debug.append("\n");
+            System.out.println(sb_debug);
+        } else if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
+            ByteBuf thisContent = content.content();
+            if (thisContent.isReadable()) {
+                buffer_body.writeBytes(thisContent);
+            }
+            if (msg instanceof LastHttpContent) {
+                sb_debug.append(buffer_body.toString(CharsetUtil.UTF_8));
+                LastHttpContent trailer = (LastHttpContent) msg;
+                if (!trailer.trailingHeaders().isEmpty()) {
+                    for (CharSequence name : trailer.trailingHeaders().names()) {
+                        sb_debug.append(name).append("=");
+                        for (CharSequence value : trailer.trailingHeaders().getAll(name)) {
+                            sb_debug.append(value).append(",");
+                        }
+                        sb_debug.append("\n\n");
+                    }
+                }
+                sb_debug.append("\n<< HTTP REQUEST -----------");
             }
         }
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(sb_debug.toString().getBytes()));
+        response.headers().set(CONTENT_TYPE, "text/plain");
+        response.headers().setInt(CONTENT_LENGTH, response.content().readableBytes());
+        ctx.write(response);
     }
 
     @Override
